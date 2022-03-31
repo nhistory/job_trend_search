d1c92e02fd8b6025d109752b1916d43257a73203
sehwan <sehwanhistory@gmail.com>
2022-03-31 12:48:54 -0300

    frontend setup

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,6 +1,12 @@
-// (function(){
-
-// })()
-
 {
+    const apiID = '0a5befcc';
+    const apiKey = '636e5e3d0dbae70083676e8b0c8a799f';
+
+    fetch(
+        `http://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=${apiID}&app_key=${apiKey}&results_per_page=20&what=javascript&content-type=application/json`
+    )
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+        });
 }
